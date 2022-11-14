699c24fab707b9aae260117c231f79a48ce50478
aliktb <aliktb@zohomail.eu>
2022-11-14 18:43:20 +0000

    added extra routes

--- routes.js
+++ routes.js
@@ -1,17 +1,43 @@
 const ROUTES = [
     {
         url: '/first',
-        auth: false,
-        creditCheck: false,
         rateLimit: {
             windowMs: 15 * 60 * 1000,
-            max: 5
+            max: 100
         },
         proxy: {
             target: process.env.API_URL,
             changeOrigin: true,
             pathRewrite: {
                 [`^/first`]: '/demo',
+            },
+        }
+    },
+    {
+        url: '/health',
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 100
+        },
+        proxy: {
+            target: process.env.API_URL,
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/health`]: '/actuator/health',
+            },
+        }
+    },
+    {
+        url: '/tasks',
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 100
+        },
+        proxy: {
+            target: process.env.API_URL,
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/tasks`]: '/tasks',
             },
         }
     }
